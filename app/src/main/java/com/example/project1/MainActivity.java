6dbeca76e9856923d37e86e71f876d6b15586ca7
David <dayjaycs49@gmail.com>
2019-06-25 21:29:04 +0900

    David First Commit

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.project1;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
+import android.widget.TableLayout;
+
+import com.google.android.material.appbar.AppBarLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TableLayout tablayout;
+    private AppBarLayout appBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +23,19 @@ public class MainActivity extends AppCompatActivity {
         // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
         TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
         ts1.setContent(R.id.content1);
-        ts1.setIndicator("TAB 1");
+        ts1.setIndicator("Contacts");
         tabHost1.addTab(ts1);
 
         // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
         TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
         ts2.setContent(R.id.content2);
-        ts2.setIndicator("TAB 2");
+        ts2.setIndicator("Gallery");
         tabHost1.addTab(ts2);
 
         // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
         TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
         ts3.setContent(R.id.content3);
-        ts3.setIndicator("TAB 3");
+        ts3.setIndicator("Tab 3");
         tabHost1.addTab(ts3);
 
         tabHost1.setCurrentTab(0);
