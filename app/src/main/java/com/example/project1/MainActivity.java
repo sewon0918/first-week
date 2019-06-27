671a3c2869557a397bea1adf97beaccf40f47be5
Sewon <sewon0918@kaist.ac.kr>
2019-06-27 16:48:49 +0900

    photo

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentUris;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TabHost;
@@ -21,6 +24,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.database.Cursor;
+
+import java.io.InputStream;
 import java.util.ArrayList;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -30,8 +35,8 @@ import java.util.Map;
 import android.widget.Button;
 import android.app.Activity;
 import java.io.Serializable;
-
-
+import android.content.ContentResolver;
+import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,97 +44,157 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     //variables for RecyclerView ?
-
     private ArrayList<String> Names = new ArrayList<>();
     private ArrayList<String> Numbers = new ArrayList<>();
+    private ArrayList<Bitmap> Photos = new ArrayList<>();
     private TableLayout tablayout;
     private AppBarLayout appBarLayout;
 
 
-    public class ContactItem implements Serializable{
+    public class ContactItem implements Serializable {
         private String user_number, user_name;
-//        private long photo_id=0, person_id=0;
+        private long photo_id=0, person_id=0;
         private int id;
-        public ContactItem(){}
-//        public long getPhoto_id(){
-//            return photo_id;
-//        }
-//        public long getPerson_id(){
-//            return person_id;
-//        }
-//        public void setPhoto_id(long id){
-//            this.photo_id=id;
-//        }
-//        public void setPerson_id(long id){
-//            this.person_id=id;
-//        }
-        public String getUser_number(){
+
+        public ContactItem() {
+        }
+        public long getPhoto_id(){
+            return photo_id;
+        }
+        public long getPerson_id(){
+            return person_id;
+        }
+        public void setPhoto_id(long id){
+            this.photo_id=id;
+        }
+        public void setPerson_id(long id){
+            this.person_id=id;
+        }
+        public String getUser_number() {
             return user_number;
         }
-        public String getUser_name(){
+
+        public String getUser_name() {
             return user_name;
         }
-        public void setId(int id){
-            this.id=id;
+
+        public void setId(int id) {
+            this.id = id;
         }
-        public int getId(){
+
+        public int getId() {
             return id;
         }
-        public void setUser_number(String string){
-            this.user_number=string;
+
+        public void setUser_number(String string) {
+            this.user_number = string;
         }
-        public void setUser_name(String string){
-            this.user_name=string;
+
+        public void setUser_name(String string) {
+            this.user_name = string;
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return this.user_number;
         }
+
         @Override
-        public int hashCode(){
+        public int hashCode() {
             return getNumberChanged().hashCode();
         }
-        public String getNumberChanged(){
-            return user_number.replace("-","");
+
+        public String getNumberChanged() {
+            return user_number.replace("-", "");
         }
+
         @Override
-        public boolean equals(Object o){
+        public boolean equals(Object o) {
             if (o instanceof ContactItem)
-                return getNumberChanged().equals(((ContactItem)o).getNumberChanged());
+                return getNumberChanged().equals(((ContactItem) o).getNumberChanged());
             return false;
         }
     }
 
-    public ArrayList<ContactItem> getContactList(){
+    public ArrayList<ContactItem> getContactList() {
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
         String[] projection = new String[]{
                 ContactsContract.CommonDataKinds.Phone.NUMBER,
                 ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-//                ContactsContract.Contacts.PHOTO_ID,
-//                ContactsContract.Contacts._ID
+                ContactsContract.Contacts.PHOTO_ID,
+                ContactsContract.Contacts._ID
         };
         String[] selectionArgs = null;
-        //String sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + "COLLATE LOCALIZED ASC";
-        Cursor cursor = getContentResolver().query(uri, projection, null, selectionArgs, null);
+        String sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC";
+        Cursor cursor = getContentResolver().query(uri, projection, null, selectionArgs, sortOrder);
         LinkedHashSet<ContactItem> hashlist = new LinkedHashSet<>();
-        if (cursor.moveToFirst()){
+        if (cursor.moveToFirst()) {
             do {
-//                long photo_id = cursor.getLong(2);
-//                long person_id = cursor.getLong(3);
+                long photo_id = cursor.getLong(2);
+                long person_id = cursor.getLong(3);
                 ContactItem contactItem = new ContactItem();
                 contactItem.setUser_number(cursor.getString(0));
                 contactItem.setUser_name(cursor.getString(1));
-//                contactItem.setPhoto_id(photo_id);
-//                contactItem.setPerson_id(person_id);
+                contactItem.setPhoto_id(photo_id);
+                contactItem.setPerson_id(person_id);
                 hashlist.add(contactItem);
-            }while (cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
         ArrayList<ContactItem> contactItems = new ArrayList<>(hashlist);
-        for (int i=0 ; i< contactItems.size(); i++){
-            contactItems.get(i).setId(i);
-        }
+//        for (int i = 0; i < contactItems.size(); i++) {
+//            contactItems.get(i).setId(i);
+//        }
         return contactItems;
     }
+
+    public Bitmap loadContactPhoto(ContentResolver cr, long id, long photo_id){
+//        Uri uri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, id);
+//        InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(cr, uri);
+//        if (input != null)
+//            return resizingBitmap(BitmapFactory.decodeStream(input));
+//        else
+//            Log.d("PHOTO", "first try failed to load photo");
+        byte[] photoBytes = null;
+        Uri photoUri = ContentUris.withAppendedId(ContactsContract.Data.CONTENT_URI, photo_id);
+        String sortOrder = ContactsContract.CommonDataKinds.Photo.PHOTO + " ASC";
+        Cursor c = cr.query(photoUri, new String[]{ContactsContract.CommonDataKinds.Photo.PHOTO}, null, null, sortOrder);
+        try{
+            if (c.moveToFirst())
+                photoBytes = c.getBlob(0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            c.close();
+        }
+        if (photoBytes != null)
+            return resizingBitmap(BitmapFactory.decodeByteArray(photoBytes, 0, photoBytes.length));
+        else
+            Log.d("PHOTO", "second try failed to load photo");
+        return null;
+    }
+    public Bitmap resizingBitmap(Bitmap oBitmap){
+        if (oBitmap == null)
+            return null;
+        float width = oBitmap.getWidth();
+        float height = oBitmap.getHeight();
+        float resizing_size = 120;
+        Bitmap rBitmap = null;
+        if (width > resizing_size){
+            float mWidth = (float) (width/100);
+            float fScale = (float) (resizing_size / mWidth);
+            width *= (fScale/100);
+            height *= (fScale/100);
+        }else if (height > resizing_size){
+            float mHeight = (float) (height/100);
+            float fScale = (float) (resizing_size / mHeight);
+            width *= (fScale/100);
+            height *= (fScale/100);
+        }
+        //Log.d("rBitmap : " + width + "," + height);
+        rBitmap = Bitmap.createScaledBitmap(oBitmap, (int)width, (int)height, true);
+        return rBitmap;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -167,30 +232,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // adding images/photos and the names of corresponding contacts to each of their own lists
-    private void initImageBitmaps(ArrayList<ContactItem> contactItems){
+    private void initImageBitmaps(ArrayList<ContactItem> contactItems) {
         Log.d(TAG, "initImageBitmaps: preparing bitmaps");
-
-        for (int j=0 ; j< contactItems.size(); j++){
+        ContentResolver cr = getContentResolver();
+        for (int j = 0; j < contactItems.size(); j++) {
             Names.add(contactItems.get(j).getUser_name());
             Numbers.add(contactItems.get(j).getUser_number());
+            Photos.add(loadContactPhoto(cr, contactItems.get(j).getPerson_id(), contactItems.get(j).getPhoto_id()));
         }
-        initRecyclerView( Names, Numbers);
+        initRecyclerView(Names, Numbers, Photos);
     }
 
-    private void initRecyclerView(ArrayList<String> Names, ArrayList<String> Numbers){
+    private void initRecyclerView(ArrayList<String> Names, ArrayList<String> Numbers, ArrayList<Bitmap> Photos) {
         Log.d(TAG, "initRecyclerView: init recyclerView.");
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(Names, Numbers, this);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(Names, Numbers, Photos,this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    }
+}
 
 
-//    public void onClick02(View v) {
-//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-5678"));
-//        startActivity(intent);
-//    }
 
 
--- app/src/main/java/com/example/project1/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/project1/RecyclerViewAdapter.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.content.Intent;
+import android.net.Uri;
+import android.graphics.Bitmap;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,14 +21,17 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private static final String TAG = "RecyclerViewAdapter";
+   // private String tel;
 
     private ArrayList<String> Names = new ArrayList<>();
     private ArrayList<String> Numbers = new ArrayList<>();
+    private ArrayList<Bitmap> Photos = new ArrayList<>();
     private Context mContext;
 
-    public RecyclerViewAdapter(ArrayList<String> Names, ArrayList<String> Numbers, Context mContext) {
+    public RecyclerViewAdapter(ArrayList<String> Names, ArrayList<String> Numbers, ArrayList<Bitmap> Photos, Context mContext) {
         this.Names = Names;
         this.Numbers = Numbers;
+        this.Photos = Photos;
         this.mContext = mContext;
     }
 
@@ -41,24 +47,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     // this is where the images and their corresponding names are loaded from layout_listitem.xml
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Log.d(TAG, "onBindViewHolder: called.");  // this is simply for helping with debugging if needed
+        //Log.d(TAG, "onBindViewHolder: called.");  // this is simply for helping with debugging if needed
 
-//        Glide.with(mContext)
-//                .asBitmap()
-//                .load(Numbers.get(position))                // this is the image URL
-//                .into(holder.image);
 
         holder.name.setText(Names.get(position));
         holder.number.setText(Numbers.get(position));
+        holder.photo.setImageBitmap(Photos.get(position));
 
+        final String tel = Numbers.get(position);
         // trying to open new page if you click a contact
-        //holder.parentLayout.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        Log.d(TAG, "onClick: clicked on: " +  mImageNames.get(position));
-
-        //    }
-        //});
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on: " +  tel);
+                //String tel = "tel" + holder.name.getText();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + tel));
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -66,22 +72,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return Names.size();
     }
 
-    @Override
-    public int getItemViewType(int position){
-        int viewType = 1;
-        return viewType;
-    }
+//    @Override
+//    public int getItemViewType(int position){
+//        int viewType = 1;
+//        return viewType;
+//    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView name;
         TextView number;
+        CircleImageView photo;
         RelativeLayout parentLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.name1);
             number = itemView.findViewById(R.id.number1);
+            photo = itemView.findViewById(R.id.photo1);
             parentLayout = itemView.findViewById(R.id.parent_layout);
         }
     }
