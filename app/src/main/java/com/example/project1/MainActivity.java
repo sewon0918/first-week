5219a38ce4395fd50156d18bc8f8fa66eda9eab2
sewon <sewon0918@kaist.ac.kr>
2019-06-28 23:37:24 +0900

    image

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.project1;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,27 +37,43 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
+    String[] permission_list = {
+            Manifest.permission.READ_CONTACTS,
+            //Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+
 
     //variables for RecyclerView ?
     private ArrayList<String> Names = new ArrayList<>();
     private ArrayList<String> Numbers = new ArrayList<>();
     private ArrayList<Bitmap> Photos = new ArrayList<>();
+    private ArrayList<Bitmap> Gallery = new ArrayList<>();
+    private ArrayList<String> imageList = new ArrayList<>();
+    private String imageEncoded;
     private TableLayout tablayout;
     private AppBarLayout appBarLayout;
 
-    private Button[][] buttons = new Button[3][3];
+    private Button[][] buttons = new Button[10][10];
     private boolean player1Turn = true;
     private int roundCount;
     private int player1Points;
     private int player2Points;
+    private int tiePoints;
     private TextView textViewPlayer1;
     private TextView textViewPlayer2;
+    private TextView textViewTie;
+    private int PICK_IMAGE_REQUEST = 1;
+    private TextView num;
+    private String imagePath;
+    private ArrayList<String> imagePathList= new ArrayList<>();
 
 
 //    public class ContactItem implements Serializable {
@@ -279,7 +305,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Log.d(TAG, "onCreate: started.");
+        checkPermission();
 
+    }
+    public void initial(){
         TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1);
         tabHost1.setup();
 
@@ -298,11 +327,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ts2.setIndicator("Gallery");
         tabHost1.addTab(ts2);
 
+        Button gallery = (Button)findViewById(R.id.button_gallery);
+        num = (TextView) findViewById(R.id.num);
+        gallery.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent();
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                // Show only images, no videos or anything else
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                // Always show the chooser (if there are multiple options available)
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
+        ts3.setContent(R.id.content3);
+        ts3.setIndicator("Game");
+        tabHost1.addTab(ts3);
+
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         textViewPlayer2 = findViewById(R.id.text_view_p2);
+        textViewTie = findViewById(R.id.text_view_tie);
 
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
                 String buttonID ="button_"+i+j;
                 int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 buttons[i][j] = findViewById(resID);
@@ -317,23 +368,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
-        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
-        ts3.setContent(R.id.content3);
-        ts3.setIndicator("Tic Tac Toe");
-        tabHost1.addTab(ts3);
-
         tabHost1.setCurrentTab(0);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try{
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+                if(data.getClipData() != null){
+                    int count = data.getClipData().getItemCount();
+                    for (int i=0; i<count; i++){
+                        Uri imageUri = data.getClipData().getItemAt(i).getUri();
+                        getImageFilePath(imageUri);
+                    }
+                }
+                else if(data.getData() != null){
+                    Uri imgUri = data.getData();
+                    getImageFilePath(imgUri);
+                }
+//                for (int i=0; i<imagePathList.size(); i++){
+//                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.parse(imagePathList.get(i)));
+//                    // Log.d(TAG, String.valueOf(bitmap));
+//                    Gallery.add(bitmap);
+//                }
+                num.setText(String.valueOf(imagePathList.size()));
+//                for (int j=0 ; j< imagePathList.size(); j++){
+//                    final String str = String.valueOf(imagePathList.size()-j);
+//                    final Bitmap bitmap1= BitmapFactory.decodeFile(imagePathList.get(j));
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            num.setText(str);
+//                            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//                            imageView.setImageBitmap(bitmap1);
+//                        }},2000);
+//                }
+
+            }else{
+                Toast.makeText(MainActivity.this, "사진 선택을 취소하였습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }catch(Exception e){
+            Toast.makeText(this, "Oops! 로딩에 오류가 있습니다.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    public void getImageFilePath(Uri uri) {
+
+        File file = new File(uri.getPath());
+        String[] filePath = file.getPath().split(":");
+        String image_id = filePath[filePath.length - 1];
+
+        Cursor cursor = getContentResolver().query(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, MediaStore.Images.Media._ID + " = ? ", new String[]{image_id}, null);
+        if (cursor!=null) {
+            cursor.moveToFirst();
+            imagePath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            imagePathList.add(imagePath);
+            cursor.close();
+        }
+    }
+    public void checkPermission(){
+        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for(String permission : permission_list){
+            //권한 허용 여부를 확인한다.
+            int chk = checkCallingOrSelfPermission(permission);
+
+            if(chk == PackageManager.PERMISSION_DENIED){
+                //권한 허용을여부를 확인하는 창을 띄운다
+                requestPermissions(permission_list,0);
+            }else{
+                initial();
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==0)
+        {
+            for(int i=0; i<grantResults.length; i++)
+            {
+                //허용됐다면
+                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+                    initial();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"앱권한설정하세요",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+    }
+
     @Override
     public void onClick(View v){
         if (!((Button) v).getText().toString().equals("")){
             return;
         }
         if (player1Turn){
-            ((Button) v).setText("X");
-        }else{
             ((Button) v).setText("O");
+        }else{
+            ((Button) v).setText("X");
         }
         roundCount++;
         if (checkForWin()){
@@ -342,69 +479,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }else{
                 player2Wins();
             }
-        }else if (roundCount == 9){
+        }else if (roundCount == 100){
             draw();
         }else{
+            updatePointsText();
             player1Turn = !player1Turn;
+            if (player1Turn){
+                textViewPlayer1.setTextColor(Color.RED);
+                textViewPlayer2.setTextColor(Color.BLACK);
+            }else{
+                textViewPlayer1.setTextColor(Color.BLACK);
+                textViewPlayer2.setTextColor(Color.RED);
+            }
+
         }
     }
     private Boolean checkForWin(){
-        String[][] field = new String[3][3];
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
+        String[][] field = new String[10][10];
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
                 field[i][j] = buttons[i][j].getText().toString();
             }
         }
-        for (int i=0; i<3; i++){
-            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")){
-                return true;
+        for (int i=0; i<10; i++){
+            for (int j=0; j<6; j++){
+                if (field[i][j].equals(field[i][j+1]) && field[i][j].equals(field[i][j+2]) && field[i][j].equals(field[i][j+3]) && field[i][j].equals(field[i][j+4]) && !field[i][j].equals("")){
+                    return true;
+                }
             }
         }
-        for (int i=0; i<3; i++){
-            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")){
-                return true;
+        for (int i=0; i<10; i++){
+            for (int j=0; j<6; j++){
+                if (field[j][i].equals(field[j+1][i]) && field[j][i].equals(field[j+2][i]) && field[j][i].equals(field[j+3][i]) && field[j][i].equals(field[j+4][i]) && !field[j][i].equals("")){
+                    return true;
+                }
             }
         }
-        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")){
-            return true;
+        for (int i=0; i<6; i++){
+            for (int j=0; j<6; j++){
+                if (field[i][j].equals(field[i+1][j+1]) && field[i][j].equals(field[i+2][j+2]) && field[i][j].equals(field[i+3][j+3]) && field[i][j].equals(field[i+4][j+4]) && !field[i][j].equals("")){
+                    return true;
+                }
+            }
         }
-        if (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals("")){
-            return true;
+        for (int i=0; i<6; i++){
+            for (int j=4; j<10; j++){
+                if (field[i][j].equals(field[i+1][j-1]) && field[i][j].equals(field[i+2][j-2]) && field[i][j].equals(field[i+3][j-3]) && field[i][j].equals(field[i+4][j-4]) && !field[i][j].equals("")){
+                    return true;
+                }
+            }
         }
         return false;
     }
     private void player1Wins(){
         player1Points++;
-        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+        Toast toast = Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,0,0);
+        toast.show();
         updatePointsText();
-        resetBoard();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                resetBoard();
+            }}, 2000);
     }
     private void player2Wins(){
         player2Points++;
-        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        Toast toast2 = Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT);
+        toast2.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,0,0);
+        toast2.show();
         updatePointsText();
-        resetBoard();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                resetBoard();
+            }},2000);
     }
     private void draw(){
+        tiePoints++;
         Toast.makeText(this, "Again!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
         resetBoard();
     }
     private void updatePointsText(){
         textViewPlayer1.setText("Player1: " + player1Points);
         textViewPlayer2.setText("Player2: " + player2Points);
+        textViewTie.setText("Tie: " + tiePoints);
     }
     private void resetBoard(){
-        for (int i=0 ; i<3 ; i++){
-            for (int j=0; j<3; j++){
+        for (int i=0 ; i<10 ; i++){
+            for (int j=0; j<10; j++){
                 buttons[i][j].setText("");
             }
         }
         roundCount=0;
         player1Turn = true;
+        textViewPlayer1.setTextColor(Color.RED);
+        textViewPlayer2.setTextColor(Color.BLACK);
     }
     private void resetGame(){
         player1Points = 0;
         player2Points = 0;
+        tiePoints = 0;
         updatePointsText();
         resetBoard();
     }
@@ -415,6 +591,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         outState.putInt("roundCount", roundCount);
         outState.putInt("player1Points", player1Points);
         outState.putInt("player2Points", player2Points);
+        outState.putInt("tie2Points", tiePoints);
         outState.putBoolean("player1Turn", player1Turn);
     }
 
@@ -424,6 +601,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         roundCount=savedInstanceState.getInt("roundCount");
         player1Points=savedInstanceState.getInt("player1Points");
         player2Points=savedInstanceState.getInt("player2Points");
+        tiePoints=savedInstanceState.getInt("tiePoints");
         player1Turn=savedInstanceState.getBoolean("player1Turn");
     }
     //////////////////////////////////////
